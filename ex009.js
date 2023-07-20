614fadec8fd4f92ec2c6d46ce0cbaa94c4b55530
Joao <jv.prado98@gmail.com>
2023-07-20 19:00:20 -0300

    Create ex009.js

---
+++ ex009.js
@@ -0,0 +1,13 @@
+const nota = 5.9;
+
+if (nota >= 9) {
+    console.log('O estudante obteve conceito A')
+}else if (nota >= 8) {
+    console.log('O estudante obteve conceito B')
+}else if (nota >= 6) {
+    console.log('O estudante obteve conceito C')
+}else if (nota >= 4) {
+    console.log('O estudante obteve conceito D')
+}else if (nota < 4 ) {
+    console.log('O estudante obteve conceito E')
+}
