45f14a34afd7db0d4d607e85c8892471abc5905e
Joao <jv.prado98@gmail.com>
2023-07-21 11:29:45 -0300

    ex011b

--- ex011.js
+++ ex011.js
@@ -1,4 +1,4 @@
-const rendaMensalEmCentavos = 300000;
+const rendaMensalEmCentavos = 150000;
 
 // Tempo decorrido de contrato. Se for maior que 60 meses, o aluno não deve mais nada.
 const mesesDecorridos = 12;
@@ -10,9 +10,11 @@ pois já quitou a dívida.
 */
 const totalJaPagoPeloAluno = 1000000;
 if (mesesDecorridos > 60) {
-    console.log('Prazo para cobrança esgotado')
+    console.log('O valor da parcela esse mês é R$0,00 reais. Nenhum valor é pois o prazo para cobrança ja expirou.')
 }else if (totalJaPagoPeloAluno / 100 > 18000) {
-    console.log('Já quitou a dívida')
+    console.log('O valor da parcela esse mês é R$0,00 reais. Nenhum valor é devido o aluno ja ter pago o valor total do curso R$18000,00 reais.')
 }else if (rendaMensalEmCentavos / 100 > 2000) {
     console.log(`O valor da parcela esse mês é R$${(rendaMensalEmCentavos / 100) * (18 / 100)} reais`)
+} else {
+    console.log('O valor da parcela esse mês é R$0,00 reais. Nenhum valor é devido pois a renda do estudante está abaixo do valor mínimo de R$2000,00 reais.')
 }
